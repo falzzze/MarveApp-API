afd7288a07377a78d50c23ec876065017a61e524
falzzze <light.falze@gmail.com>
2023-04-09 04:20:24 +0300

    add comicPage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,27 @@
-import { useState } from "react";
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
+import AppHeader from "../appHeader/AppHeader";
 
 
 const App = () => {
 
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <RandomChar />
-        <div className="char__content">
-          <CharList onCharSelected={onCharSelected} />
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-        <AppBanner/>
-        <ComicsList/>
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
+            <Route path="/" element={<MainPage />}/>
+            <Route path="/comics" element={<ComicsPage />}/>
+            <Route path="/comics/:comicId" element={<SingleComicPage />}/>
+            {/* <Route path="*">
+              <Page404 />
+            </Route> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
