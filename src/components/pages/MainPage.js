9ab0ca466f704a0d47565c90529af967c1cea32e
falzzze <light.falze@gmail.com>
2023-04-14 21:32:21 +0300

    add form for search char

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import CharSearchForm from "../CharSearchForm/CharSearchForm";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -24,9 +25,14 @@ const MainPage = () => {
         <ErrorBoundary>
           <CharList onCharSelected={onCharSelected} />
         </ErrorBoundary>
+        <div>
         <ErrorBoundary>
           <CharInfo charId={selectedChar} />
         </ErrorBoundary>
+        <ErrorBoundary>
+          <CharSearchForm />
+        </ErrorBoundary>
+        </div>
       </div>
       <img className="bg-decoration" src={decoration} alt="vision" />
     </>
